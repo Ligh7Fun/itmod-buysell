d1b37ae871e8473b7e16059cf2214075c0395ec1
Denis Tsybin <m5221710@gmail.com>
2024-04-10 18:48:50 +0500

    add new section

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
+
+		const targetId = this.getAttribute('href')
+		const targetElement = document.querySelector(targetId)
+		const headerHeight = 82 // Высота фиксированной шапки
+		const targetPosition = targetElement.offsetTop - headerHeight
+
+		// Плавный скролл
+		window.scrollTo({
+			top: targetPosition,
+			behavior: 'smooth',
+		})
+	})
+})
+
+window.addEventListener('scroll', function () {
+	let image = document.querySelector('.hero-scroll-down')
+	let scrollValue = window.scrollY
+	let opacity = 1
+
+	if (scrollValue > 10 && scrollValue <= 200) {
+		opacity = 1 - (scrollValue - 10) / 190
+	}
+
+	if (scrollValue > 200) {
+		opacity = 0
+	}
+
+	image.style.opacity = opacity.toString()
+})
